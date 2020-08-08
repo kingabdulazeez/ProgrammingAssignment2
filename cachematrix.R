9fba3f24925ae703e15b5500cc49557043bfc091
kingabdulazeez <69373281+kingabdulazeez@users.noreply.github.com>
2020-08-07 19:00:05 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+## Put comments here that give an overall description of what you
 ## functions do
 
 ## Write a short comment describing this function
